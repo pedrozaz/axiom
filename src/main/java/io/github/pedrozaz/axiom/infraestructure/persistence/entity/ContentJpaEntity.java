84c814517c1dc94a3be493c41a33531437412ccf
Guilherme <guilhermebarb0sa@proton.me>
2025-06-26 10:57:58 -0300

    Fixing bugs from kafka and spring security for tests

--- src/main/java/io/github/pedrozaz/axiom/infraestructure/persistence/entity/ContentJpaEntity.java
+++ src/main/java/io/github/pedrozaz/axiom/infraestructure/persistence/entity/ContentJpaEntity.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class ContentJpaEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, columnDefinition = "TEXT")
